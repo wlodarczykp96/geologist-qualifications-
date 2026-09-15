a43791ff87630764ca51da6203c805587d0154ba
wlodarczykp96 <wlodarczykp96@gmail.com>
2026-09-16 00:00:20 +0200

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import random
 import time
+import streamlit.components.v1 as components
 # ==============================================================================
 # HASŁO ADMINISTRATORA
 # ==============================================================================

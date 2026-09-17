554502efd7490bc83b42493f67f99b775fffdf60
wlodarczykp96 <wlodarczykp96@gmail.com>
2026-09-17 22:03:40 +0200

    app.py

--- app.py
+++ app.py
@@ -677,19 +677,25 @@ elif menu_glowne == "🎮 Testy i Nauka":
                  if st.button("🏁 Zakończ Test / Egzamin"):
                         st.session_state.test_zakonczony = True
 
-               if "Egzamin" in st.session_state.aktywny_tryb:
-                    punkty = sum(
-                    1 for i, q in enumerate(lista)
-                    if set(st.session_state.odpowiedzi_egzamin.get(i, [])) == set(q["poprawne"])
-                    )
+if st.button("🏁 Zakończ Test / Egzamin"):
+    st.session_state.test_zakonczony = True
 
-                    zapisz_wynik_egzaminu(
-                        st.session_state.zalogowany_uzytkownik,
-                        st.session_state.aktywny_tryb,
-                        st.session_state.wybrana_baza,
-                        punkty,
-                        len(lista)
-                    )     
+        if "Egzamin" in st.session_state.aktywny_tryb:
+            punkty = sum(
+                1 for i, q in enumerate(lista)
+                if set(st.session_state.odpowiedzi_egzamin.get(i, [])) == set(q["poprawne"])
+            )
+
+            zapisz_wynik_egzaminu(
+                st.session_state.zalogowany_uzytkownik,
+                st.session_state.aktywny_tryb,
+                st.session_state.wybrana_baza,
+                punkty,
+                len(lista)
+            )
+
+        st.success("🎉 Egzamin zakończony! Wynik został zapisany.")
+        st.balloons()
         else:
             st.balloons()
             st.success("🎉 Zakończyłeś test / egzamin!")

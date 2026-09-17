e1045aad5ff67f43c19f362a68e57405db192205
wlodarczykp96 <wlodarczykp96@gmail.com>
2026-09-17 20:07:45 +0200

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 import streamlit.components.v1 as components
 
 # W app.py pobieramy dane z osobnego pliku bazy_danych.py:
-from bazy_danych import BAZY_PYTAN
+from baza_danych import BAZY_PYTAN
 
 # I inicjalizujemy je w sesji w ten sposób:
 if 'bazy' not in st.session_state:
